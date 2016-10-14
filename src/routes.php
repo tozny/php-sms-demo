4aabb9fd2f6df49f901392da64cc9b376e61215a
Eric Mann <eric@tozny.com>
2016-10-14 14:08:15 -0700

    Scaffold out the templates

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,18 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/send', function($request, $response, $args) {
+    return $this->renderer->render($response, 'sent.phtml', $args);
+});
+
+$app->post('/validate', function($request, $response, $args) {
+    return $this->renderer->render($response, 'valid.phtml', $args);
 });
