212f9b39d5594349cecd8a32746054b2994ea242
Eric Mann <eric@tozny.com>
2016-10-14 14:31:32 -0700

    Initial walkthrough of the various steps.

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,6 +3,10 @@
 
 $container = $app->getContainer();
 
+// Set up environment
+$dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
 // view renderer
 $container['renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
@@ -16,4 +20,12 @@ $container['logger'] = function ($c) {
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
+};
+
+// Tozny
+$container['tozny_realm'] = function ($c) {
+    return new Tozny_Remote_Realm_API(getenv('REALM_KEY_ID'), getenv('REALM_SECRET'), getenv('TOZNY_API'));
+};
+$container['tozny_user'] = function($c) {
+    return new Tozny_Remote_User_API(getenv('REALM_KEY_ID'), getenv('TOZNY_API'));
 };
--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,10 @@
 <?php
+use \Slim\Http\Response;
+use \Slim\Http\Request;
+
 // Routes
 
-$app->get('/', function ($request, $response, $args) {
+$app->get('/', function (Request $request, Response $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
@@ -9,10 +12,32 @@ $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->post('/send', function($request, $response, $args) {
+$app->post('/send', function(Request $request, Response $response, $args) use ($app) {
+    $destination = $request->getParam('destination');
+    if ( empty( $destination ) ) {
+        return $response->withRedirect('/');
+    }
+    $sent = $this->tozny_realm->realmOTPChallenge( null, 'sms-otp-6', $destination, null, 'verify' );
+
+    if ( 'ok' !== $sent['return'] ) {
+        return $response->withRedirect('/');
+    }
+
+    $args['session'] = $sent['session_id'];
+
     return $this->renderer->render($response, 'sent.phtml', $args);
 });
 
-$app->post('/validate', function($request, $response, $args) {
+$app->post('/validate', function(Request $request, Response $response, $args) {
+    $otp = $request->getParam('otp');
+    $session = $request->getParam('session');
+    if ( empty( $otp ) || empty( $session ) ) {
+        return $response->withRedirect('/send');
+    }
+
+    $validated = $this->tozny_user->userOTPResult( $session, $otp );
+
+    $args['validated'] = $validated;
+
     return $this->renderer->render($response, 'valid.phtml', $args);
 });
